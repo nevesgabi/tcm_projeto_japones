fdb34f9f5e1557d0674b74f228c3e11aa0c864d1
nevesgabi <gabineves2003@gmail.com>
2020-11-24 12:12:04 -0300

    first commit

---
+++ projeto_tcm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projeto_tcm.MvcApplication" Language="C#" %>
---
+++ projeto_tcm/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_tcm.Models
+{
+    public class UserLogin
+    {
+        public int cad_usuario { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome do aluno!!")]
+        [Display(Name = "Usuário:  ")]
+        public string nome_usuario { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome do aluno!!")]
+        [Display(Name = "Senha:  ")]
+        public string senha { get; set; }
+    }
+}
