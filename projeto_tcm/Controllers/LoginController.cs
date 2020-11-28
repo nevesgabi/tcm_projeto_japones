404a6d18679b16bf08ec6720427935ce30b8b55e
nevesgabi <gabineves2003@gmail.com>
2020-11-28 16:32:06 -0300

    Corrigido o login

--- projeto_tcm/Controllers/LoginController.cs
+++ projeto_tcm/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using projeto_tcm.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace projeto_tcm.Controllers
 {
@@ -10,6 +12,25 @@ namespace projeto_tcm.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult VerificarUsuario(UserLogin u)
+        {
+            if (u.nome_usuario != null && u.senha != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.nome_usuario, false);
+                Session["usuario"] = u.nome_usuario.ToString();
+                Session["senha"] = u.senha.ToString();
+
+                //direcionar o usuario para a página index da home
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
             return View();
         }
     }
